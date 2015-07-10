dc63aae766f54dddd15487661e6b49ba8e7b170b
Radu <radu.bordei89@gmail.com>
2015-07-10 11:15:28 +0300

    on get show form to upload module, post on processing.

--- routes.php
+++ routes.php
@@ -1,16 +1,20 @@
 <?php
 
 use Flysap\ModuleManger\Contracts\ModuleServiceContract;
+use Illuminate\Http\Request;
 
 Route::group(['prefix' => 'module-manager'], function() {
 
     /**
      * Route for module installation
      */
-    Route::post('upload', ['as' => 'module-upload', function() {
+    Route::match(['upload', 'get'], '/upload', ['as' => 'module-upload', function(Request $request) {
         $service = app(ModuleServiceContract::class);
 
-        return $service->install();
+        if( $request->method() == 'post' )
+            return $service->install();
+
+        return view('module-manager::upload');
     }]);
 
     Route::get('list/{page?}', function() {
