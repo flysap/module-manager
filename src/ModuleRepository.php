d61314ceec34bc6e61fe3bcfc810667ecd5845df
Radu <radu.bordei89@gmail.com>
2015-07-10 10:29:54 +0300

    adding some of scopes .

--- src/ModuleRepository.php
+++ src/ModuleRepository.php
@@ -6,10 +6,31 @@ use Illuminate\Database\Eloquent\Model;
 
 class ModuleRepository extends Model {
 
+    use ScopeTrait;
+
     public $table = 'modules';
 
     public $timestamps = false;
 
-    public function modules() {}
+    /**
+     * Get all modules .
+     *
+     * @return \Illuminate\Database\Eloquent\Collection|static[]
+     */
+    public function modules() {
+        return $this
+            ->all();
+    }
+
+    /**
+     * Is module installed .
+     *
+     * @param $name
+     * @return mixed
+     */
+    public function isInstalled($name) {
+        return $this->ofName($name)
+            ->first();
+    }
 
 }
---
+++ src/ScopeTrait.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Flysap\ModuleManger;
+
+trait ScopeTrait {
+
+    /**
+     * Get all active modules .
+     *
+     * @param $query
+     * @return mixed
+     */
+    public function scopeActive($query) {
+        return $query
+            ->where('active', 1);
+    }
+
+    /**
+     * Get all inactive modules .
+     *
+     * @param $query
+     * @return mixed
+     */
+    public function scopeInactive($query) {
+        return $query
+            ->where('active', 0);
+    }
+
+    /**
+     * Get module by name .
+     *
+     * @param $query
+     * @param $name
+     * @return mixed
+     */
+    public function scopeOfName($query, $name) {
+        return $query->where('name', $name);
+    }
+}
