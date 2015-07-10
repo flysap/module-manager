a73d98c8bd125d6b142421f7cddee0afe7542326
Radu <radu.bordei89@gmail.com>
2015-07-10 12:39:37 +0300

    fix.

--- resources/views/upload.blade.php
+++ resources/views/upload.blade.php
@@ -1,5 +1,6 @@
 <form enctype="multipart/form-data" method="post" action="{{route('module-upload')}}">
     <input type="file" name="module">
 
+    <input type="hidden" name="_token" value="{{csrf_token()}}">
     <input type="submit" value="Upload">
 </form>
