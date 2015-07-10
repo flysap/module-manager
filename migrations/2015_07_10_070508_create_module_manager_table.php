d06ae44da7f6e8e14e9b4daabc5e3df0b1f3e67f
Radu <radu.bordei89@gmail.com>
2015-07-10 11:04:32 +0300

    fix migration

--- migrations/2015_07_10_070508_create_module_manager_table.php
+++ migrations/2015_07_10_070508_create_module_manager_table.php
@@ -11,7 +11,7 @@ class CreateModuleManagerTable extends Migration {
      * @return void
      */
     public function up() {
-        Schema::table('modules', function(Blueprint $table) {
+        Schema::create('modules', function(Blueprint $table) {
             $table->increments('id');
             $table->string('name');
             $table->decimal('version');
