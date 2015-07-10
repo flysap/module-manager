ccc21d028e3aebd71ecfb9af1fe9343c97dc940a
Radu <radu.bordei89@gmail.com>
2015-07-10 10:19:07 +0300

    implement module uploader.

--- src/ModuleUploader.php
+++ src/ModuleUploader.php
@@ -2,8 +2,11 @@
 
 namespace Flysap\ModuleManger;
 
+use Flysap\ModuleManger\Exceptions\ModuleUploaderException;
 use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\Finder\Finder;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use ZipArchive;
 
 /**
  * Module uploader processing ..
@@ -39,6 +42,11 @@ class ModuleUploader {
      */
     protected $finder;
 
+    /**
+     * @var array
+     */
+    protected $extensions = ['zip'];
+
     public function __construct(FileSystem $fileSystem, Finder $finder) {
         $this->fileSystem = $fileSystem;
         $this->finder     = $finder;
@@ -47,9 +55,46 @@ class ModuleUploader {
     /**
      * Upload module to modules path ..
      *
-     * @param $module
+     * @param UploadedFile $module
+     * @return \Symfony\Component\HttpFoundation\File\File
+     * @throws ModuleUploaderException
      */
-    public function upload($module) {
+    public function upload(UploadedFile $module) {
+
+        if(! in_array( $this->extensions, $module->guessClientExtension() ))
+            throw new ModuleUploaderException(
+                _('Invalid module format.')
+            );
+
+        if(! $this->isValid($module))
+            throw new ModuleUploaderException(_("Error: can't get module info file."));
+
+        $storagePath = $this->getStoragePath();
+
+        /** Check if path exists, other case create one . */
+        if( $this->fileSystem->exists(
+            $storagePath
+        ) )
+            $this->fileSystem->mkdir(
+                $storagePath
+            );
+
+        /** Change mode . */
+        $this->fileSystem->chmod(
+            $storagePath, 0777, false
+        );
+
+
+        if( ! $uploaded = $module->move(
+            $storagePath
+        ) )
+            throw new ModuleUploaderException(_("Error on upload module."));
+
+        $uploaded = $this->extract(
+            $uploaded
+        );
+
+        return $uploaded;
 
         #@todo
          /**
@@ -63,5 +108,50 @@ class ModuleUploader {
           */
     }
 
+    /**
+     * Get storage path .
+     *
+     * @return mixed
+     * @throws ModuleUploaderException
+     */
+    protected function getStoragePath() {
+        $path = config('module-manager::module_path');
+
+        if(! $path || $path == '')
+            throw new ModuleUploaderException(
+                _("Cannot fine storage path for modules.")
+            );
+
+        return $path;
+    }
+
+    /**
+     * Validate module .
+     *
+     * @param UploadedFile $module
+     * @return bool
+     * @throws ModuleUploaderException
+     */
+    protected function isValid(UploadedFile $module) {
+        $zip = new ZipArchive();
+
+        if ($zip->open($module))
+            $moduleInfo = $zip->getStream('module.info');
+
+        if(! $moduleInfo)
+            return false;
+
+        return true;
+    }
+
+    /**
+     * Extract module archive .
+     *
+     * @param $uploaded
+     * @param null $path
+     */
+    protected function extract($uploaded, $path = null) {
+        return $uploaded;
+    }
 
 }
