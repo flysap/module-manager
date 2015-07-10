0c488ef2a9285b799ea9ec4c65198f1a2136e150
Radu <radu.bordei89@gmail.com>
2015-07-10 10:36:31 +0300

    adding some of routes.

--- routes.php
+++ routes.php
@@ -1,5 +1,27 @@
 <?php
 
+use Flysap\ModuleManger\Contracts\ModuleServiceContract;
+
 Route::group(['prefix' => 'module-manager'], function() {
 
+    /**
+     * Route for module installation
+     */
+    Route::post('upload', function() {
+        $service = app(ModuleServiceContract::class);
+
+        return $service->install();
+    });
+
+    Route::get('list/{page?}', function() {
+        $service = app(ModuleServiceContract::class);
+
+        return $service->list();
+    });
+
+    Route::get('remove/{module}', function() {
+        $service = app(ModuleServiceContract::class);
+
+        return $service->remove();
+    });
 });
