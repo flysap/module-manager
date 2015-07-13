75a0e2c325b38fe688ed41a5d93c6414edf14b59
Radu <radu.bordei89@gmail.com>
2015-07-13 10:21:57 +0300

    addin parser config class.

---
+++ src/ParserIni.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Flysap\ModuleManger;
+
+use Flysap\ModuleManger\Contracts\ConfigParserContract;
+
+class ParserIni implements ConfigParserContract {
+
+    /**
+     * Parse file ..
+     *
+     * @param $file
+     * @return array
+     */
+    public function parse($file) {
+        return parse_ini_string(
+            $file
+        );
+    }
+}
