5d098dee76c2d0c81273ff314cc2d6c4ca11709e
Radu <radu.bordei89@gmail.com>
2015-09-21 14:30:34 +0300

    use find Modules.

--- src/ModuleService.php
+++ src/ModuleService.php
@@ -55,7 +55,7 @@ class ModuleService {
 
     public function lists() {
         $modules = $this->cacheManager
-            ->getModules();
+            ->findModules();
 
         $table = TableManager\table(array(
             'columns' => array('name' => ['closure' => function($value) {
